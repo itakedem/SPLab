8f97cd1b480c554779bc896230a8837bd577f1ea
itamarkedem-student <itamarked@wix.com>
2021-10-25 19:03:08 +0300

    task1c done?

--- espl/lab1/task1/task1c/task1c.c
+++ espl/lab1/task1/task1c/task1c.c
@@ -59,6 +59,41 @@ void printLowHighChar(char c)
         fprintf(out, "%c", c);
     }
 }
+
+void char2ascii(int c)
+{
+    do
+    {
+        fprintf(out, "%d ", c);
+    } while ((c = fgetc(in)) != '\n');
+}
+
+void char2Binary(int c)
+{
+    int arr[8];
+    int i;
+    do
+    {
+        num2Binary(c, arr);
+        for (i = 0; i < 8; i++)
+            fprintf(out, "%d", arr[i]);
+        fprintf(out, " ");
+
+    } while ((c = fgetc(in)) != '\n');
+}
+
+
+void num2Binary(int n, int arr[8])
+{
+    int i = 7;
+    while (i >= 0)
+    {
+        arr[i] = n & 1;
+        n >>= 1;
+        i--;
+    }
+}
+
 void resolver(int argc, char* task, char ** argv)
 {
     int i = 1;
