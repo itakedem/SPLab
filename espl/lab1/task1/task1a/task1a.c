d53a861d953e5e0059a66e87ea47c0ef5d0c067f
itamarkedem-student <itamarked@wix.com>
2021-10-25 18:11:33 +0300

    fix?

--- espl/lab1/task1/task1a/task1a.c
+++ espl/lab1/task1/task1a/task1a.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 
+void char2ascii(int c, FILE* in, FILE* out)
+{
+	do
+	{
+		fprintf(out, "%d ", c);
+	} while ((c = fgetc(in)) != '\n');
+}
+
 int main(int argc, char **argv)
 {
     char c;
     while ((c = fgetc(stdin)) != EOF)
     {
-        fprintf(stdout, "%d ", c);
-        fprintf(stdout, '\n');
+        fprintf(out, "%d ", c);
+        fprintf(stdout, "\n");
     }
 
     return 0;
