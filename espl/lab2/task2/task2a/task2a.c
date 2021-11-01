592ed49b4246fb4663338ecfa20fe31b4e957251
itamarkedem-student <itamarked@wix.com>
2021-11-01 17:48:19 +0200

    task2a ready

---
+++ espl/lab2/task2/task2a/task2a.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define BUFFERSIZE 10
+
+
+FILE * in;
+FILE * out;
+
+void echo_printer(char c); /* task 0 from lab1 - prints c to the standard output */
+void ascii_printer(char c); /* task 1a from lab1 - print the ASCII code of c to the standard output*/
+void binary_printer(char c); /* task 1b from lab1 – print the binary representation of c to the standard output */
+void lower_to_upper_printer(char c); /* task 1c from lab1 – print c to the standard output in upper case */
+
+
+void echo_printer(char c)
+{
+    fprintf(stdout, "%c", c);
+}
+void ascii_printer(char c)
+{
+    fprintf(stdout, "%d ", c);
+}
+
+void num2Binary(int n, int arr[8])
+{
+    int i = 7;
+    while (i >= 0)
+    {
+        arr[i] = n & 1;
+        n >>= 1;
+        i--;
+    }
+}
+
+void binary_printer(char c)
+{
+    int arr[8];
+    num2Binary(c, arr);
+    arr_printer(arr, 8);
+}
+
+void lower_to_upper_printer(char c)
+{
+    if(islower(c))
+    {
+        fprintf(out, "%c", toupper(c));
+    }
+    else if(isupper(c))
+    {
+        fprintf(out, "%c", tolower(c));
+    }
+    else
+    {
+        fprintf(out,"%c", c);
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    in = stdin;
+    out = stdout;
+    return 0;
+}
