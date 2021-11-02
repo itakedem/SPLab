da039ef53b59e119b09cbcff134f7920d5fb40b2
itamarkedem-student <itamarked@wix.com>
2021-11-02 17:37:59 +0200

    fix task0b first try

--- espl/lab3/task0/task0b.c
+++ espl/lab3/task0/task0b.c
@@ -3,13 +3,13 @@
 
 int main(int argc, char **argv)
 {
-    FILE* file = fopen(argv[1]);
+    FILE* file = fopen(argv[1], "r");
     char buffer[8];
     while (fread(buffer, 8, sizeof(char), file) == sizeof(char))
     {
         printf("%s", buffer);
     }
 
-    close(file);
+    fclose(file);
     return 0;
 }
