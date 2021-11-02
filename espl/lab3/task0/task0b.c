b1dde35f695724eefe1eb2419a5a10688e094ff2
itamarkedem-student <itamarked@wix.com>
2021-11-02 17:41:47 +0200

    task0b 3nd try

--- espl/lab3/task0/task0b.c
+++ espl/lab3/task0/task0b.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
     FILE* file = fopen(argv[1], "r");
-    char buffer[8];
+    char* buffer;
     while (fread(buffer, 8, sizeof(char), file) == sizeof(char))
     {
         printf("%X", buffer &0xff);
