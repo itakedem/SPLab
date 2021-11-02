605714a51efa352b191978f4d51d2c14992db71b
itamarkedem-student <itamarked@wix.com>
2021-11-02 17:44:30 +0200

    task0b 4th try

--- espl/lab3/task0/task0b.c
+++ espl/lab3/task0/task0b.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int main(int argc, char **argv)
-{
-    FILE* file = fopen(argv[1], "r");
-    char* buffer;
-    while (fread(buffer, 8, sizeof(char), file) == sizeof(char))
-    {
-        printf("%X", buffer &0xff);
+int PrintHex(char buffer[], size_t length){
+    for (size_t i = 0; i < (length - 1); i++){
+        printf("%02hhX ", buffer[i]);
     }
-    printf("\n")
+    printf("%02hhX\n", buffer[length - 1]);
+    return 0;
+}
 
-    fclose(file);
+int main(int argc, char **argv){
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+
+    if (fp == NULL){
+        printf("\nError:! cannot open file");
+        exit(1);
+    }
+    fseek(fp, 0, SEEK_END);
+    size_t length = (long)ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    char *buffer;
+    buffer = (char *)malloc(length);
+    fread(buffer, 1, length, fp);
+    PrintHex(buffer, length);
+    free(buffer);
+    fclose(fp);
     return 0;
 }
