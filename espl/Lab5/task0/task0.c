f004db3d7e61ccbabeca009bc5d9dd6de3f1aeda
itamarkedem-student <itamarked@wix.com>
2021-11-27 12:00:24 +0200

    fixed make task0

--- espl/Lab5/task0/task0.c
+++ espl/Lab5/task0/task0.c
@@ -8,12 +8,12 @@ int main(int argc, char const *argv[])
     if (argc != 2)
         return 1;
 
-    int file_descriptor = open_file(argv[1], 0);        //0 means readOnly
+    int file_descriptor = open_file(argv[1], 0);
     printf("the file descriptor is %d\n", file_descriptor);
 
     int status;
     if ((status = close_file(file_descriptor)) >= 0)
-        printf("CLOSED FILE\n")
+        printf("CLOSED FILE\n");
     else
         printf("ERROR [%d]", status);
 
