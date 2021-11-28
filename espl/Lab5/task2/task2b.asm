773c48c4070b769fc4cd86c76a9fd4edae6fe582
Tal Dan <dta@post.bgu.ac.il>
2021-11-28 18:24:53 +0200

    tali's task1b barely works

--- espl/Lab5/task2/task2b.asm
+++ espl/Lab5/task2/task2b.asm
@@ -1,11 +1,14 @@
 %include "cmpstr.asm"
-%include "task1b.asm"
+%include "utoa_s.asm"
 
 section .data
-    num_lines_str: db 'Number of lines: ', 0
-    num_lines_str_len: equ  $ - num_lines_str        ; calc string size
-    l_opt: db '-l', 0
-    endl: db 10, 0
+    lineSpace: db 10, 0
+    flagW: db '-w', 0
+    flagWs: db '-ws', 0
+    wordGloble: db 0
+    endOfWord: db 0, 0
+
+    format db '%d', 10, 0
 
     open_file_err: db 'Failed to open the file', 10, 0
     open_file_err_len: equ  $ - open_file_err
@@ -17,13 +20,20 @@ section .data
     read_from_file_err_len: equ  $ - read_from_file_err
 
     write_to_File_err: db 'Failed to write to file', 10, 0
-    write_to_File_err_len: equ  $ -  write_to_File_er
+    write_to_File_err_len: equ  $ -  write_to_File_err
 
 section .bss
     int_str_repr: resb 11                            ; save place for convertion of int to str
 
 section .text
     global _start
+    extern printf
+    global read
+    global write
+    global open
+    global close
+    global strlen
+    
 
 _start:
 
@@ -35,9 +45,6 @@ _start:
     push eax
 
     call	main
-    jmp .exit
-
-
 
 .exit:
     mov ebx,eax
@@ -47,36 +54,420 @@ _start:
 %define ARGC dword [ebp + 8]
 %define ARGV dword [ebp + 12]
 %define BUFFER_SIZE dword 50
-%define O_RDONLY dword 0
-%define LINE_NUM dword [ebp - 58]
-%define IS_COUNT_LINES byte [ebp - 59]
-%define PATH_TO_F dword [ebp - 53]
+%define flag byte [ebp-55]
+%define fileName dword [ebp-59]
+%define wordsCounter dword [ebp-63]
+%define wordToCount dword [ebp-67]
 
 main: 
-    enter 63, 0                 ; reserve 50 bytes fro buffer starting at ebp-50
-                                ; 4 to save fd, 4 to save num of lines, 
-                                ; 1 for is '-w' option flag, and 4 for path to file pointer
-
-    mov LINE_NUM, 0                                  ; put 0 in stack in case we need to count lines
-    mov IS_COUNT_LINES, 0                            ; flag for -w option
-
-parse_args:
-    mov ecx, ARGC                                       
-    sub ecx, 1                                       ; now ecx number of cmd args
+    enter 67, 0         ; 50 bytes for buffer starting at ebp-50 
+                        ; 4 to file fd
+                        ; 1 flag
+                        ; 4 fileName
+                        ; 4 wordsCounter
+                        ; 4 wordToCount
 
     mov ebx, ARGV
+    add ebx, 4              ; move ebx to point on argv[1] 
+    mov flag, 0             ; default for no flags
+    mov wordsCounter, 1     ; init counter
 
-.move_ebx:
-    add ebx, 4                                       ; now ebx points at argv[1] - which is the file arg
-
-    push l_opt                                       ; push l as first arg to cmpstr
-    push dword [ebx]                                 ; push [ebx] as second arg to cmpstr
-
-    call cmpstr
-
-    pop edx
-    pop edx                                          ; clear arguments
-
+    push flagW
+    push dword [ebx]  
+    call cmpstr        
+    pop ecx
+    pop ecx 
     cmp eax, 0
-    je .found_l
-    jmp .fount_not_l
+    je .rememberFlagW
+
+    push flagWs
+    push dword [ebx]  
+    call cmpstr        
+    pop ecx
+    pop ecx 
+    cmp eax, 0
+    je .rememberFlagWs
+
+    jmp .openFile
+
+.rememberFlagW:
+    add ebx, 4              ; ebx points to file name
+    mov flag, 1             ; remember flag -w
+    jmp .openFile
+
+.rememberFlagWs:
+    mov flag, 2             ; remember flag -ws
+
+    add ebx, 4 
+    mov edx, [ebx]
+    mov wordToCount, edx
+    mov [wordGloble], edx
+    add ebx, 4              ; ebx points to file name
+
+.openFile:
+    mov edx, [ebx]          ; now ebx contains the file name
+    mov fileName, edx       ; store file name
+    push 0                  ;read only flag
+    push edx
+    call open               ; fd at eax if succeed
+    pop ebx
+    pop ebx                 ; clean stack
+
+    mov [ebp - 54], eax    ; save fd on stack
+    cmp eax, 0
+    jle .finishError            ; if eax <= 0 finish with error
+
+.readFile:
+    push BUFFER_SIZE        ; arg1 for read - size to read
+    mov ebx, ebp
+    sub ebx, 50             ; ebx points buffer start  
+    push ebx                ; arg2 for read - buffer
+    push dword [ebp - 54]   ; arg3 for read - fd
+    call read
+    pop ebx
+    pop ebx
+    pop ebx                 ; clean stack
+
+    cmp eax, -1             ; check if error
+    jle .finishError
+
+    cmp eax, 0               ; check if read something
+    jne .act
+
+.finishRead:
+    push dword [ebp - 54]  ; arg1 to close - fd
+    call close
+    pop ebx                 ; clean stack
+    cmp flag, 1
+    jge .printCounter
+    jmp .finishSuccess
+
+.act:
+    cmp flag, 1
+    je .countWords         
+    cmp flag, 2
+    je .countWordToCount 
+
+;else - no flag so printing file
+.printBuffer:
+    push eax                ; arg1 to write - num of bytes (got from read)
+    mov ebx, ebp
+    sub ebx, 50             ;ebx points at buffer start
+    push ebx                ; arg2 to write - buffer
+    push 1                  ; arg3 to write - stdout fd 
+    call write
+    pop ebx
+    pop ebx
+    pop ebx                 ; clean stack
+
+    cmp eax, 0              ; check if wrote something
+    jle .finishError
+
+    jmp .readFile           ; continue reading
+
+.countWords:
+    push eax                 ; num of bytes (got from read)
+    mov ebx, ebp
+    sub ebx, 50
+    push ebx                  ; buffer address
+    mov ebx, ebp
+    sub ebx, 63
+    push ebx                   ; counter adress
+    call countWords
+    pop ebx
+    pop ebx
+    pop ebx                                          ; clean stack from args
+
+    jmp .readFile
+
+.countWordToCount:
+    mov ebx, ebp
+    sub ebx, 67                 ;wordToCount pointer
+    push ebx
+    push eax                 ; num of bytes (got from read)
+    mov ebx, ebp
+    sub ebx, 50
+    push ebx                  ; buffer address
+    mov ebx, ebp
+    sub ebx, 63
+    push ebx                   ; counter adress
+    call countWordToCount
+    pop ebx
+    pop ebx
+    pop ebx
+    pop ebx                                          ; clean stack from args
+
+    jmp .readFile
+
+.finishError:
+    mov eax, 1              ; default error return
+    jmp _start.exit             ; return
+
+.printCounter:
+    mov eax, wordsCounter
+    mov ebx, int_str_repr
+    push ebx
+    push eax                ; arg1 to otua_s - counter
+    call utoa_s             ; calling utoa to get string of our counter
+    pop ecx                 ; stack clean
+    pop ecx
+
+    ;counts number of digits in counter as string
+    push int_str_repr
+    call strlen
+    pop edx
+
+    push eax                ; arg1 to write - num of bytes 
+    push dword int_str_repr                ; arg2 to write - counter as string
+    push 1                  ; arg3 to write - stdout fd 
+    call write
+    pop ebx
+    pop ebx
+    pop ebx                 ; clean stack
+
+    cmp eax, 0              ; check if wrote something
+    jle .finishError
+
+    .finishSuccess:
+    ; print line space
+    push dword 1                ; arg1 to write - num of bytes 
+    push dword lineSpace                ; arg2 to write - counter as string
+    push 1                  ; arg3 to write - stdout fd 
+    call write
+    pop ebx
+    pop ebx
+    pop ebx                 ; clean stack
+    
+    mov eax, 0              ; default success return
+
+.return:
+    leave
+    ret
+
+read:
+	push ebp 	;stack maintenance
+	push ebx 	;keeping prev values
+	push ecx 	;keeping prev values
+	push edx 	;keeping prev values
+	mov ebp, esp	 ;stack maintenance
+	mov eax, 3			;read system call num
+	mov ebx, [ebp + 20]	;file descriptor arg
+	mov ecx, [ebp + 24]	;input buffer arg
+	mov edx, [ebp + 28]	;buffer size arg
+	int 0x80			;call kernel
+    cmp eax, -1
+    jle .readFailed
+
+.FINISH_READ:
+	mov esp, ebp	 ;stack maintenance
+	pop edx			;return prev scope values
+	pop ecx			;return prev scope values
+	pop ebx			;return prev scope values
+	pop ebp 		;stack maintenance
+	ret 			;stack maintenance
+
+.readFailed:
+    push dword  read_from_file_err_len               ; buffer pointer
+    push dword  read_from_file_err                   ; number of bytes read to buffer
+    push 1                                           ; fd to stdout
+    call write
+    pop ebx 
+    pop ebx 
+    pop ebx                                          ; clean stack of arguments
+    jmp main.finishError
+
+write:
+	push ebp 	;stack maintenance
+	push ebx 	;keeping prev values
+	push ecx 	;keeping prev values
+	push edx 	;keeping prev values
+	mov ebp, esp	 ;stack maintenance
+	mov eax, 4			;write system call num
+	mov ebx, [ebp + 20]	;file descriptor arg
+	mov ecx, [ebp + 24]	;output buffer arg
+	mov edx, [ebp + 28]	;count of bytes to sent arg
+	int 0x80			;call kernel
+    cmp eax, -1
+    jle .writeFailed
+
+.FINISH_WRITE:
+	mov esp, ebp	 ;stack maintenance
+	pop edx			;return prev scope values
+	pop ecx			;return prev scope values
+	pop ebx			;return prev scope values
+	pop ebp 		;stack maintenance
+	ret 			;stack maintenance
+
+.writeFailed:
+    push dword  write_to_File_err_len                ; buffer pointer
+    push dword  write_to_File_err                    ; number of bytes read to buffer
+    push 1                                           ; fd to stdout
+    call write
+    pop ebx 
+    pop ebx 
+    pop ebx                                          ; clean stack of arguments
+    jmp main.finishError
+
+open:
+    enter 0, 0
+    push ebx           ; save regs
+    push ecx           ; save regs
+    mov eax, 5h        ; setup system call
+    mov ebx, [ebp + 8] ; setup system call
+    mov ecx, [ebp + 12] ; setup system call
+    int 0x80           ; sys call
+    cmp eax, -1
+    jle .openFailed
+    pop ecx            ; restore regs
+    pop ebx            ; restore regs
+    leave
+    ret
+
+.openFailed:
+    mov ebx, open_file_err
+    push dword  open_file_err_len                    ; buffer pointer
+    push dword  open_file_err                        ; number of bytes read to buffer
+    push 1                                           ; fd to stdout
+    call write
+    pop ebx 
+    pop ebx 
+    pop ebx                                          ; clean stack of arguments
+    jmp main.finishError
+
+close:
+    enter 0, 0
+    push ebx           ; save regs
+    mov eax, 6h        ; setup system call
+    mov ebx, [ebp + 8]
+    int 0x80           ; sys call
+    cmp eax, -1
+    jle .closeFailed
+    pop ebx            ; restore regs
+    leave
+    ret                ; clear args from stack and return
+
+.closeFailed:
+    push dword  close_file_err_len                   ; buffer pointer
+    push dword  close_file_err                       ; number of bytes read to buffer
+    push 1                                           ; fd to stdout
+    call write
+    pop ebx 
+    pop ebx 
+    pop ebx                                          ; clean stack of arguments
+    jmp main.finishError
+
+strlen:
+    enter 0, 0
+    push ebx
+    mov eax, -1
+
+strlen_body:
+    inc eax            ; calc offset
+    mov ebx, eax
+    add ebx, [ebp+8]   ; take str from stack
+    mov bl, BYTE [ebx] ; get byte at adress of ebx
+    test bl, bl        ; check if byte is \0
+    jne strlen_body      ; if not \0 cont to next byte
+    pop ebx
+    leave
+    ret
+
+%define COUNTER dword [ebp + 8]
+%define BUFFER_P dword [ebp + 12]
+%define BUF_SIZE dword [ebp + 16]
+countWords:
+    enter 0,0
+    push ecx
+    push ebx
+    push eax
+
+    mov ecx, BUF_SIZE
+    mov ebx, BUFFER_P
+
+.countLoop:
+    call .incrementIfNeeded
+    inc ebx                                          ; move pointer
+    loop .countLoop
+    jmp .finish
+
+.incrementIfNeeded:
+    cmp [ebx], byte 10                               ; 10 in ascii means new line
+    je .increment                                    
+    cmp [ebx], byte 32                               ; 32 in ascii means space, new word
+    je .increment
+    
+    ret
+
+.increment:
+    mov eax, COUNTER                                 ; get counter adress
+    inc dword [eax]
+    ret
+
+.finish:
+    pop eax
+    pop ebx
+    pop ecx
+    leave
+    ret
+
+%define COUNTER2 dword [ebp + 8]
+%define BUFFER_P2 dword [ebp + 12]
+%define BUF_SIZE2 dword [ebp + 16]
+%define WORD_TO_COUNT dword [ebp + 20]
+
+countWordToCount:
+    enter 0,0
+    push edx
+    push ecx
+    push ebx
+    push eax
+
+    mov ecx, BUF_SIZE2
+    mov ebx, BUFFER_P2
+    mov [edx], ebx            ; save start of word to compare
+
+.countWordToCountLoop:
+    call .dealNextChar
+    loop .countWordToCountLoop
+    jmp .finishWordToCount
+
+.dealNextChar:
+    cmp [ebx], byte 32
+    je .incrementIfNeededWordToCount
+    inc ebx                     ; next char if not space
+    ret
+
+.incrementIfNeededWordToCount:
+                                ;padding with \0 both wordToCount and wordToCompare
+
+    mov eax, [wordGloble]
+    push eax                
+    push dword [edx]        ; where does it end ?
+    call cmpstr             ; ?????????????????????????????????????????
+    pop edx
+    pop edx 
+    
+    inc ebx                     ; next char if last one was space
+    mov [edx], ebx            ; move edx to point the start of the next word
+    
+    cmp eax, 0
+    je .incrementWordToCount
+    ret
+
+
+
+.incrementWordToCount:
+    mov eax, COUNTER2                                 ; get counter adress
+    mov dl, [eax]
+    mov ah,2
+    int 21h
+    inc dword [eax]
+    ret
+
+.finishWordToCount:
+    pop eax
+    pop ebx
+    pop ecx
+    pop edx
+    leave
+    ret
