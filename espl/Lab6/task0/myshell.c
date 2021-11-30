21f8214e39851f7809bd788f618002d7ca379ba9
itamarkedem-student <itamarked@wix.com>
2021-11-30 17:29:46 +0200

    finished task0?

--- espl/Lab6/task0/myshell.c
+++ espl/Lab6/task0/myshell.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <linux/limits.h>
+#include <string.h>
+
+#include <LineParser.h>
+
+char currDir[PATH_MAX];
+char userText[2048];
 
 int main(int argc, char** argv)
 {
-    char currDir[PATH_MAX];
     getcwd(currDir, sizeof(currDir));
-    printf("%s%s",currDir, "$\n");
+    while (1)
+    {
+        printf("%s$ ", currDir);
+        fgets(userText, stdin);
+        cmdLine currLine = parseSingleCmdLine(&userText);
+        execute(currLine);
+        printf("\n");
+        if (cmpstr(currLine.arguments[0], "quit") == 0)
+        {
+            freeCmdLines(currLine);
+            printf("end of loop");      /*TODO: remove */
+            break;
+        }
+        freeCmdLines(currLine);
+    }
+}
+
+void execute(cmdLine* currLine)
+{
+    int ans = execv(currDir, currLine->arguments);
+    if (ans == -1)
+        perror("There was an error");
+
 }
