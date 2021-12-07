1f3f6cf91995ae7aeb3570cedafa8d43c4cd652c
itamarkedem-student <itamarked@wix.com>
2021-12-07 18:01:48 +0200

    wtf??

---
+++ espl/Lab7/task2/mypipeline.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MSG_SIZE 16
+#define MAX_ARGS 256 
+
+char buffer[MSG_SIZE];
+char* msg1;
+char* msg2;
+char** args1;
+char** args2;
+
+
+int main(int argc, char** argv)
+{
+    int pid, p[2], status1, status2;
+    char* msg = "ls -l | tail -n 2";
+
+    parseStr(msg);
+
+    if (pipe(p) < 0)
+        _exit(1);
+
+    if ((pid = fork()) == 0)
+    {
+        close(STDOUT_FILENO);
+        dup(p[1]);
+        close(p[1]);
+        int ans = excvp(args1);
+
+        _exit(ans);
+    }
+    else
+    {
+        close(p[1]);
+    }
+
+    if ((pid = fork()) == 0)
+    {
+        close(STDIN_FILENO);
+        dup(p[0]);
+        close(p[0]);
+        int ans = execvp(args2);
+
+        _exit(ans);
+    }
+    else
+    {
+        close(p[0]);
+    }
+
+    waitpid(pid, &status1, 0);
+    waitpid(pid, &status2, 0);
+
+    printf("finished\n");
+
+    return 0;
+}
+
+void parseStr(char* input)
+{
+    char* delimiter = "|";
+
+    msg1 = strtok(input, delimiter);
+    msg2 = strtok(NULL, delimiter);
+
+    msg2 = msg2 + 1;
+    parseArgs(msg1, args1);
+    parseArgs(msg2, args2);
+}
+
+void parseArgs(char* msg, char** arg)
+{
+    char* delimiter2 = " ";
+    int i = 0;
+    result = strtok( msg, delimiter);
+    while( result)
+    {
+        arg[i++] = strClone(result);
+        result = strtok(NULL, delimiter);
+    }
+    arg[i] = NULL;
+}
