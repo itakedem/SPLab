16f56288973509cdcc4a09632a0723c4d26b4f4c
itamarkedem-student <itamarked@wix.com>
2021-12-07 13:15:42 +0200

    fixes task0

--- espl/Lab7/task0/myPipe.c
+++ espl/Lab7/task0/myPipe.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define MSGSIZE = 16
+#define MSGSIZE 16
 
 
 int main(int argc, char** argv)
 {
-    int pid, p[2], nBytes;
+    int p[2];
     char buffer[MSGSIZE];
     char* msg = "hello";
 
     if (pipe(p) < 0)
-        exit(1);
+        _exit(1);
 
-    if ((pid = fork()) == 0)
+    if (fork() == 0)
         write(p[1], msg, MSGSIZE);
     else
-        while ((nBytes = read(p[0], buffer, MSGSIZE)) > 0)
+        while ((read(p[0], buffer, MSGSIZE)) > 0)
             printf("%s\n", buffer);
     printf("finished");
 }
