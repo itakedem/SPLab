64d76491b42320b6f09935dc6b5f69220a77b3dc
itamarkedem-student <itamarked@wix.com>
2021-12-07 13:20:21 +0200

    fixes task0

--- espl/Lab7/task0/myPipe.c
+++ espl/Lab7/task0/myPipe.c
@@ -18,6 +18,7 @@ int main(int argc, char** argv)
     else
     {
         waitpid(pid, &status, 0);
+        read(p[0], buffer, MSGSIZE);
         printf("%s\n", buffer);
         printf("finished");
     }
