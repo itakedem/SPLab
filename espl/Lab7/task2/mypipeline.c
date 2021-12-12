6df15646c032589ebb13baa159d7f4af8639a55e
itamarkedem-student <itamarked@wix.com>
2021-12-12 17:46:04 +0200

    maybe task2

--- espl/Lab7/task2/mypipeline.c
+++ espl/Lab7/task2/mypipeline.c
@@ -19,74 +19,44 @@ void parseArgs(char* msg, char** arg);
 int main(int argc, char** argv)
 {
     int pid, p[2], status1, status2;
-    char* msg = "ls -l | tail -n 2";
-
-    parseStr(msg);
 
     if (pipe(p) < 0)
         _exit(1);
 
     if ((pid = fork()) == 0)
     {
+        char* msg1[3] = {"ls","-l",NULL};
         close(STDOUT_FILENO);
         dup(p[1]);
         close(p[1]);
-        int ans = execvp(args1[0], args1);
+        char* buff[]
+        int ans = execvp("ls", msg1);
 
         _exit(ans);
     }
     else
     {
         close(p[1]);
-    }
 
-    if ((pid = fork()) == 0)
-    {
-        close(STDIN_FILENO);
-        dup(p[0]);
-        close(p[0]);
-        int ans = execvp(args2[0], args2);
+        if ((pid = fork()) == 0)
+        {
+            close(STDIN_FILENO);
+            dup(p[0]);
+            close(p[0]);
+            char* msg2[4] = {"tail", "-n", "2", NULL};
+            int ans = execvp("tail", msg2);
 
-        _exit(ans);
+            _exit(ans);
+        }
+        else
+        {
+            close(p[0]);
+            waitpid(pid, &status1, 0);
+            waitpid(pid, &status2, 0);
+        }
     }
-    else
-    {
-        close(p[0]);
-    }
-
-    waitpid(pid, &status1, 0);
-    waitpid(pid, &status2, 0);
 
     printf("finished\n");
 
     return 0;
-}
-
-void parseStr(char* input)
-{
-    int delimiter = '|';
-    char* rest = input;
-
-    char* ind = strchr(input, delimiter);
-    input[ind] = 0;
-    msg1 = strtok_r(rest, delimiter, &rest);
-    msg2 = strchr(input, delimiter);
-
-    msg2 = msg2 + 1;
-    parseArgs(msg1, args1);
-    parseArgs(msg2, args2);
-}
-
-void parseArgs(char* msg, char** arg)
-{
-    char* delimiter = " ";
-    int i = 0;
-    char* rest = msg;
-    char* result ;
-
-    while((result = strtok_r(rest, delimiter, &rest)))
-    {
-        arg[i++] = result;
-    }
-    arg[i] = NULL;
 }
