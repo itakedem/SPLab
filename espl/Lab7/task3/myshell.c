cb442f907ee57b39d29d51b3d2ac6ed3504fab63
itamarkedem-student <itamarked@wix.com>
2021-12-13 16:10:28 +0200

    fixed!

--- espl/Lab7/task3/myshell.c
+++ espl/Lab7/task3/myshell.c
@@ -78,16 +78,16 @@ int main(int argc, char** argv)
 
 void addToHistory(char* line)
 {
-    if (isMaxhistory == 1)
-    {
-        free(history[pointerHistory]);
-    }
     char* newLine= strClone(line);
+
+    if (isMaxhistory == 1)
+        free(history[pointerHistory]);
+
     history[pointerHistory++] = newLine;
 }
 
 int commands(cmdLine* line)
-{
+//{
     char* command = line->arguments[0];
     if (strcmp("cd", command) == 0)
         return executeCD(line);
