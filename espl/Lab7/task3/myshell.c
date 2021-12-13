3df62b4e505707bd5475e534f7bb1532f1024711
itamarkedem-student <itamarked@wix.com>
2021-12-13 16:55:42 +0200

    revert

--- espl/Lab7/task3/myshell.c
+++ espl/Lab7/task3/myshell.c
@@ -78,11 +78,11 @@ int main(int argc, char** argv)
 
 void addToHistory(char* line)
 {
-    char* newLine= strClone(line);
-
     if (isMaxhistory == 1)
+    {
         free(history[pointerHistory]);
-
+    }
+    char* newLine= strClone(line);
     history[pointerHistory++] = newLine;
 }
 
