b5384105b27ac81bcf4f87b941d9e7af59667a32
itamarkedem-student <itamarked@wix.com>
2021-12-20 17:16:01 +0200

    fixed 3

--- espl/Lab8/task3/task3.c
+++ espl/Lab8/task3/task3.c
@@ -8,39 +8,39 @@
 #include <sys/types.h>
 #include <libgen.h>
 
-// int fib(int limit){
-//   int a = 0;
-//   int b = 1;
-//   int printed = 0;
-//   if (limit < 1) return -1;
-//   while(a < limit){
-//     printed = a;
-//     a = b;
-//     b = printed+b;
-//   }
-//   return printed;
-// }
+ int fib(int limit){
+   int a = 0;
+   int b = 1;
+   int printed = 0;
+   if (limit < 1) return -1;
+   while(a < limit){
+     printed = a;
+     a = b;
+     b = printed+b;
+   }
+   return printed;
+ }
 
 
 
-int fib(int limit)
-{
-    int a = 0, b = 1, temp = 1, last_a = -1, i = 0;
-
-    while (a < limit) {
-        i += 1;
-        last_a = a;
-        temp += a;
-        a = b;
-        b = temp;
-    }
-
-    // just some garbage that does nothing to waste space here
-    for (i = 0; i < limit; i++);
-    for (i = 0; i < limit; i++);
-
-    return last_a;
-}
+//int fib(int limit)
+//{
+//    int a = 0, b = 1, temp = 1, last_a = -1, i = 0;
+//
+//    while (a < limit) {
+//        i += 1;
+//        last_a = a;
+//        temp += a;
+//        a = b;
+//        b = temp;
+//    }
+//
+//    // just some garbage that does nothing to waste space here
+//    for (i = 0; i < limit; i++);
+//    for (i = 0; i < limit; i++);
+//
+//    return last_a;
+//}
 
 int main(int argc, char** argv) {
   printf("%d\n",fib(atoi(argv[1])));
