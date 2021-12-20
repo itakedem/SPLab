702160d91f3d8f50dcd56aa2f2d54cb82fe01f65
itamarkedem-student <itamarked@wix.com>
2021-12-20 17:43:02 +0200

    switched patch

--- espl/Lab8/task3/patch.c
+++ espl/Lab8/task3/patch.c
@@ -1,57 +1,37 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <linux/limits.h>
-#include <unistd.h>
-#include <sys/wait.h> 
+#include <stdio.h>
 #include <sys/types.h>
-#include <libgen.h>
+#include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
-int calc_size(int fd)
-{
-    struct stat st;
-    fstat(fd, &st);
-    return st.st_size;
-}
+// params: ./patch fib 1325 66 fibonacci 1613
+int main(int argc, char **argv) {
+    char *source_file = argv[1];
+    int source_pos = atoi(argv[2]);
+    size_t size = atoi(argv[3]);
+    char *target_file = argv[4];
+    int target_pos = atoi(argv[5]);
 
-int patch(char* source_file, int source_pos, int size, char* dest_file, int dest_pos){
-  int fdsrc = open(source_file, O_RDONLY, 0);
-  int fdest = open(dest_file, O_RDWR, 0x0777);
-      if (fdest == -1)
-    {
-        perror("Error opening file for writing");
-        exit(EXIT_FAILURE);
-    }  
-  char* srcFile = (char *)mmap(NULL, calc_size(fdsrc), PROT_READ, MAP_SHARED, fdsrc, 0);
-  char* destFile = (char *)mmap(NULL, calc_size(fdest), PROT_READ | PROT_WRITE, MAP_SHARED, fdest, 0);
-     if (destFile == MAP_FAILED)
-    {
-        close(fdest);
-        perror("Error mmapping the file");
-        exit(EXIT_FAILURE);
-    }
-  char* copy = (char*)malloc(size);
-  memcpy(copy, srcFile + source_pos, size);
+    int source_fd = open(source_file, O_RDONLY);
+    struct stat source_st;
+    fstat(source_fd, &source_st);
+    size_t source_size = source_st.st_size;
 
-  for (int i = 0; i < size; i++)
-    {
-        int pos = i + dest_pos;
-        
-        destFile[pos] = copy[i];
-    }
-  msync(destFile, calc_size(fdsrc), MS_SYNC);
-  msync(destFile, calc_size(fdest), MS_SYNC);
-  close(fdsrc);
-  close(fdest);
-  free(copy);
-  return 0;
-}
+    int target_fd = open(target_file, O_RDWR);
+    struct stat target_st;
+    fstat(target_fd, &target_st);
+    size_t target_size = target_st.st_size;
 
-int main(int argc, char** argv) {
-  patch(argv[1], atoi(argv[2]), atoi(argv[3]), argv[4], atoi(argv[5]));
-  return 0;
+    char *source_content = mmap(NULL, source_size, PROT_READ, MAP_PRIVATE, source_fd, 0);
+    char *target_content = mmap(NULL, target_size, PROT_READ | PROT_WRITE, MAP_SHARED, target_fd, 0);
+
+    memcpy(target_content + target_pos, source_content + source_pos, size);
+
+    close(source_fd);
+    close(target_fd);
+
+    return 0;
 }
