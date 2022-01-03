b1cc89beb713b08e43b8a1841f6a1889f7dc00fb
itamarkedem-student <itamarked@wix.com>
2022-01-03 16:02:45 +0200

    neat and clean

--- espl/Lab10/trys.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if __name__ == "__main__":
-    for (root, dirs, files) in os.walk('./', topdown=True):
-        print("hello")
-        print(root)
-        print(dirs)
-        print(files)
